8e3f3521bbc63e04228c30b53a568e251f92e7d3
frank008 <frank.vei@outlook.com>
2021-04-23 17:33:30 +0800

    定时器demo

---
+++ src/main/java/com/jobs/QuartzSimple.java
@@ -0,0 +1,10 @@
+package com.jobs;
+
+public class QuartzSimple {
+    public static void main(String[] args) {
+        //简单任务调度，每隔多少时间执行一次，执行n次
+        SchedulerUtil.handleSimpleTrigger("44033", "3333", "44033", "3333", MyJob.class, 1, 8);
+        //复杂调度，每天的什么时候执行任务
+        SchedulerUtil.hadleCronTrigger("44033", "3333", "44033", "3333", MyJob.class, "0 19 19 * * ? *");
+    }
+}
---
+++ src/main/java/com/jobs/TestTask.java
@@ -0,0 +1,22 @@
+package com.jobs;
+
+//import org.quartz.JobExecutionContext;
+//import org.quartz.JobExecutionException;
+//import org.springframework.scheduling.quartz.QuartzJobBean;
+//import org.apache.log4j.Logger;
+//
+//public class TestTask extends QuartzJobBean{
+//
+//    Logger log = Logger.getLogger( PolicyServiceTest.class );
+//
+//    public void executeAction(){
+//        log.info("Hello quartz");
+//    }
+//
+//    @Override
+//    protected void executeInternal(JobExecutionContext arg0)
+//            throws JobExecutionException {
+//
+//    }
+//
+//}
